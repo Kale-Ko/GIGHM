76e691dade1fbadb5db5b149925f43a3676ed648
Kale-Ko <github@kaleko.dev>
2022-05-06 18:56:22 -0400

    Add version headers, fix a bug, and add the ability to change the clear color

--- src/main/java/io/github/kale_ko/gighm/scene/GameObject.java
+++ src/main/java/io/github/kale_ko/gighm/scene/GameObject.java
@@ -11,6 +11,7 @@ import io.github.kale_ko.gighm.scene.components.Transform;
 /**
  * An object that can hold different components
  * 
+ * @version 1.0.0
  * @since 1.0.0
  */
 public class GameObject {
--- src/main/java/io/github/kale_ko/gighm/scene/components/Transform.java
+++ src/main/java/io/github/kale_ko/gighm/scene/components/Transform.java
@@ -11,6 +11,7 @@ import org.joml.Vector3f;
 /**
  * An object transform
  * 
+ * @version 1.0.0
  * @since 1.0.0
  */
 public class Transform extends Component {
