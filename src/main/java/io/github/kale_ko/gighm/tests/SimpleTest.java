f662c0cb6c39578bb13d7c4598977d942a60e786
Kale-Ko <github@kaleko.dev>
2022-05-11 21:26:02 -0400

    Add Shader#loadDefault to load the default shader, fix some issues with textures, and clean up javadocs

---
+++ src/main/java/io/github/kale_ko/gighm/tests/SimpleTest.java
@@ -0,0 +1,35 @@
+package io.github.kale_ko.gighm.tests;
+
+import java.io.IOException;
+import io.github.kale_ko.gighm.rendering.Renderer;
+import io.github.kale_ko.gighm.rendering.Window;
+import io.github.kale_ko.gighm.rendering.shaders.Shader;
+import io.github.kale_ko.gighm.rendering.shaders.ShaderLoader;
+import io.github.kale_ko.gighm.scene.GameObject;
+import io.github.kale_ko.gighm.scene.Scene;
+import io.github.kale_ko.gighm.scene.components.Camera;
+
+public class SimpleTest {
+    public static void main(String[] args) {
+        try {
+            int width = 800;
+            int height = 600;
+            int farPlane = 512;
+
+            Scene scene = new Scene();
+
+            GameObject cameraObject = new GameObject();
+            Camera camera = Camera.createOrthagraphic(width, height, farPlane); // OR createPerspective(fov, width / height, nearPlane, farPlane);
+            cameraObject.addComponent(camera);
+            scene.addObjects(cameraObject);
+
+            Shader shader = ShaderLoader.loadDefault();
+            Renderer renderer = new Renderer(scene, camera, shader);
+
+            new Window(renderer, "Test Renderer", width, height);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
