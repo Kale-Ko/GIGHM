bc72299758d95ac1067420585ca31cf734878efe
Kale-Ko <github@kaleko.ga>
2022-05-15 21:07:11 -0400

    Add unimplimented tick event

---
+++ src/main/java/io/github/kale_ko/gighm/events/types/physics/TickEvent.java
@@ -0,0 +1,18 @@
+package io.github.kale_ko.gighm.events.types.physics;
+
+import io.github.kale_ko.gighm.events.types.Event;
+
+/**
+ * A tick event
+ * 
+ * @version 1.7.0
+ * @since 1.7.0
+ */
+public class TickEvent extends Event {
+    /**
+     * Create a tick event
+     * 
+     * @since 1.7.0
+     */
+    public TickEvent() { }
+}
