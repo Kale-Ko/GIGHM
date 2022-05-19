529f8ef4ac871af54124cc144c11628caa925271
Kale-Ko <github@kaleko.dev>
2022-05-18 22:47:40 -0400

    Add referance back to the scene in GameObject, add GameObject#hasComponent, and add GameObject#getComponents

--- src/main/java/io/github/kale_ko/gighm/scene/GameObject.java
+++ src/main/java/io/github/kale_ko/gighm/scene/GameObject.java
@@ -1,6 +1,8 @@
 package io.github.kale_ko.gighm.scene;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.joml.Quaterniond;
 import org.joml.Vector3d;
@@ -10,10 +12,17 @@ import io.github.kale_ko.gighm.scene.components.Transform;
 /**
  * An object that can hold different components
  * 
- * @version 1.0.0
+ * @version 1.7.0
  * @since 1.0.0
  */
 public class GameObject {
+    /**
+     * The scene this gameobject is a part of
+     * 
+     * @since 1.7.0
+     */
+    private Scene scene = null;
+
     /**
      * The name of the game object
      * 
@@ -73,6 +82,17 @@ public class GameObject {
     }
 
     /**
+     * Get all the components from the object
+     * 
+     * @return All the components from the object
+     * 
+     * @since 1.7.0
+     */
+    public List<Component> getComponents() {
+        return new ArrayList<>(this.components.values());
+    }
+
+    /**
      * Add a component to the object
      * 
      * @param object The object to add
@@ -81,8 +101,27 @@ public class GameObject {
      * @since 1.0.0
      */
     public <T extends Component> void addComponent(T object) {
-        object._setGameObject(this);
-        this.components.put(object.getClass(), object);
+        if (object.getGameObject() == null) {
+            object._setGameObject(this);
+
+            this.components.put(object.getClass(), object);
+        } else {
+            throw new RuntimeException("You can't add a component to multiple gameobjects");
+        }
+    }
+
+    /**
+     * Get weather a component exists on the object
+     * 
+     * @param clazz The type to check
+     * @param <T> The type to check
+     * 
+     * @return Weather a component exists on the object
+     * 
+     * @since 1.7.0
+     */
+    public <T extends Component> boolean hasComponent(Class<T> clazz) {
+        return getComponent(clazz) != null;
     }
 
     /**
@@ -110,5 +149,27 @@ public class GameObject {
      */
     public <T extends Component> void removeComponent(Class<T> clazz) {
         this.components.remove(clazz);
+    }
+
+    /**
+     * Get the scene this component is a part of
+     * 
+     * @return The scene this component is a part of
+     * 
+     * @since 1.0.0
+     */
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    /**
+     * Set the scene this component is a part of (Only used internally)
+     * 
+     * @param scene The scene this component should be a part of
+     * 
+     * @since 1.0.0
+     */
+    public void _setScene(Scene scene) {
+        this.scene = scene;
     }
 }
